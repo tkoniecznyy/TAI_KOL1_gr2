89577483ca1c58e2772cb7ca79f479d284684150
tkoniecznyy <konieczny.t@icloud.com>
2021-04-09 15:39:01 +0200

    commit

---
+++ navbar.js
@@ -0,0 +1,4 @@
+let nav = '<nav><a href="index.html">Home</a>' + '<a href="contact.html">Contact</a></nav>';
+
+let navbarElem = document.querySelector(".navbar");
+navbarElem.innerHTML = nav;
---
+++ script.js
@@ -0,0 +1,8 @@
+
+    function changeColor() {
+    document.getElementById("box").style.backgroundColor = "yellow";
+}
+    function changeColor2() {
+        document.getElementById("box").style.backgroundColor = "green";
+    }
+
